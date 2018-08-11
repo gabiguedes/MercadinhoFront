93bb36b382c7aa4688f47ee6a37ff08ef7436f02
azooiverde <zooiv3rde@gmail.com>
2018-08-11 18:07:05 -0300

    adicioando produtos, categoria, perfil

--- pages/categoria/categoria.js
+++ pages/categoria/categoria.js
@@ -1,15 +1,22 @@
-(function($){
-	$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-	  if (!$(this).next().hasClass('show')) {
-		$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-	  }
-	  var $subMenu = $(this).next(".dropdown-menu");
-	  $subMenu.toggleClass('show');
+class Categoria {
 
-	  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-		$('.dropdown-submenu .show').removeClass("show");
-	  });
+	constructor() {
 
-	  return false;
+	}
+}
+
+(function ($) {
+	$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+		if (!$(this).next().hasClass('show')) {
+			$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+		}
+		var $subMenu = $(this).next(".dropdown-menu");
+		$subMenu.toggleClass('show');
+
+		$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+			$('.dropdown-submenu .show').removeClass("show");
+		});
+
+		return false;
 	});
 })(jQuery)
